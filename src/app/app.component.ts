045bdfaee08d5190a63836419d4f524780e09fb2
raymoo42 <turzer.stefan@gmail.com>
2024-06-10 09:27:06 +0200

    more stuffs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, ToastModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [MessageService]
 })
 export class AppComponent {
-  title = 'contact-form-challenge';
+
+  constructor(private messageService: MessageService) { }
+
+  wasSubmittedPrior = false
+
+  contactForm = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.compose([Validators.email, Validators.required])),
+    queryType: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+    consent: new FormControl(false, Validators.requiredTrue),
+  })
+
+  onSubmit(): void {
+    this.wasSubmittedPrior = true;
+
+    if (this.contactForm.valid) {
+      this.messageService.add({ severity: "success", summary: "Message Sent", detail: "Thanks for completing the form. We'll be in touch soon!" })
+    }
+  }
 }
